390b3add9995dd62339a054ecf4f6dab911c38d3
Fanny Guillemont <fanny.gt10@gmail.com>
2024-02-26 21:00:47 +0100

    feat: filter cards with categories and tags

--- src/app/cards-list/cards-list.page.ts
+++ src/app/cards-list/cards-list.page.ts
@@ -9,8 +9,13 @@ import {CardsService} from "../services/cards.service";
 })
 export class CardsListPage implements OnInit {
   cards: Card[] = [];
+  filteredCards: Card[] = [];
+  categories: (number | string)[] = [1, 2, 3, 4, 5, 6, 7, 'Done'];
+  selectedCategories: (number | string)[] = [];
+  lastSearchTag: string = '';
 
-  constructor(private cardsService: CardsService) {}
+  constructor(private cardsService: CardsService) {
+  }
 
   ngOnInit() {
     this.fetchCards();
@@ -19,11 +24,42 @@ export class CardsListPage implements OnInit {
   fetchCards(tags?: string[]) {
     this.cardsService.getAllCards(tags).subscribe(cards => {
       this.cards = cards;
+      this.filteredCards = [...this.cards];
     });
   }
 
   searchTags(tag: string) {
-    this.cards = this.cards.filter(card => card.tag?.toLowerCase().includes(tag.toLowerCase()));
+    this.lastSearchTag = tag;
+    this.filterCards();
   }
 
+  getCategory(category: string): number | string {
+    const mapping: { [key: string]: number | string } = {
+      'FIRST': 1,
+      'SECOND': 2,
+      'THIRD': 3,
+      'FOURTH': 4,
+      'FIFTH': 5,
+      'SIXTH': 6,
+      'SEVEN': 7,
+      'DONE': 'Done',
+    };
+    return mapping[category] || 'Done';
+  }
+
+  filterCards() {
+    let tempFilteredCards = [...this.cards];
+
+    if (this.lastSearchTag) {
+      tempFilteredCards = tempFilteredCards.filter(card => card.tag?.toLowerCase().includes(this.lastSearchTag.toLowerCase()));
+    }
+
+    if (this.selectedCategories.length > 0) {
+      tempFilteredCards = tempFilteredCards.filter(card =>
+        this.selectedCategories.includes(this.getCategory(card.category))
+      );
+    }
+
+    this.filteredCards = tempFilteredCards;
+  }
 }
